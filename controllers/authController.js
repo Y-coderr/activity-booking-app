74eaba07dd52038ec2ced3ff55415dfbcb06f111
Yash Kadam <kadamyash6112@gmail.com>
2025-05-20 13:04:02 +0530

    implementing JWT tokens

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
-const bcrypt = require("bcryptjs");
+const bcrypt = require("bcrypt");
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
 
 // @route   POST /api/auth/register
 // @desc    Register a new user
@@ -31,6 +32,28 @@ exports.registerUser = async (req, res) => {
 
   } catch (error) {
     console.error("Registration error:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Check if user exists
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: "Invalid credentials" });
+
+    // Compare passwords
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ message: "Invalid credentials" });
+
+    // Generate JWT
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1d" });
+
+    res.status(200).json({ token, user: { name: user.name, email: user.email } });
+  } catch (err) {
+    console.error("Login error:", err);
     res.status(500).json({ message: "Server error" });
   }
 };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser } = require("../controllers/authController");
+const { registerUser, loginUser } = require("../controllers/authController");
+
 
 // @route   POST /api/auth/register
 router.post("/register", registerUser);
+router.post("/login", loginUser);
 
 module.exports = router;
